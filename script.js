dbe5e6b4a1ef1e6082931adce9a3080a042a82ad
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-07 00:25:25 +0300

    Get Weather Data From API

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const locationInput = document.querySelector('input');
 const submitBtn = document.querySelector('#submitBtn');
 
-let locationValue = null;
+let queryUrl;
+let myKey = '28SUAPEDEBK3W6FMPLKTFMRFY';
 
 submitBtn.addEventListener('click', getLocation)
 function getLocation() {
-  locationValue = locationInput.value;
+  let locationValue = locationInput.value;
   locationInput.value = '';
   if (!locationValue) {
     console.log('location can not be empty');
     return;
   }
+  queryUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationValue}?key=${myKey}`
+  console.log(queryUrl);
+}
+
+submitBtn.addEventListener('click', getWeatherData);
+async function getWeatherData() {
+  let response = await fetch(queryUrl, {mode: 'cors'});
+  response = await response.json();
+  console.log(response);
 }
