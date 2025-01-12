e9073951ecef75848cd7e4feedde5eaea9c3925c
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-12 12:58:02 +0300

    Cache Dom Elements In A Module
    Makes code appear more organized

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const locationInput = document.querySelector('input');
-const submitBtn = document.querySelector('#submitBtn');
-const todayDiv = document.querySelector('#today');
-const forecastDiv = document.querySelector('#forecast');
-const forecastInput = document.querySelector('#forecastLength');
-const weatherInfo = document.querySelector('#weatherInfo');
-const heading = document.querySelector('#heading');
 let errorDivs = [];
+
+(function domElements() {
+  const locationInput = document.querySelector('input');
+  const submitBtn = document.querySelector('#submitBtn');
+  const todayDiv = document.querySelector('#today');
+  const forecastDiv = document.querySelector('#forecast');
+  const forecastInput = document.querySelector('#forecastLength');
+  const weatherInfo = document.querySelector('#weatherInfo');
+  const heading = document.querySelector('#heading');
+
+  return {
+    locationInput,
+    submitBtn,
+    todayDiv,
+    forecastDiv,
+    forecastInput,
+    weatherInfo,
+    heading,
+  }
+})();
 
 let queryUrl;
 let weatherKey = '28SUAPEDEBK3W6FMPLKTFMRFY';
